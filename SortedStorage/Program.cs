c8af86b9dd948f5231b988465c96f46e7fb80f2f
Tiago Deliberali Santos <tiago.deliberali@gmail.com>
2021-07-28 01:18:05 -0300

    Add custom events to application insight on azure

--- SortedStorage/Program.cs
+++ SortedStorage/Program.cs
@@ -1,18 +1,27 @@
-﻿using SortedStorage.Adapter.Out;
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.EventCounterCollector;
+using SortedStorage.Adapter.Out;
 using SortedStorage.Application;
 using SortedStorage.TcpServer;
 using System;
-using System.IO;
-using System.Reflection;
 using System.Threading.Tasks;
 
 namespace SortedStorage
 {
     class Program
     {
+        private static EventCounterCollectionModule _events = new EventCounterCollectionModule();
+
         static async Task Main(string[] args)
         {
-            string path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            if (args.Length < 2)
+            {
+                throw new ArgumentException("Should pass path and InstrumentationKey as parameters");
+            }
+
+            SetupTelemetry(args[1]);
+
+            string path = args[0];
             Console.WriteLine($"[{nameof(Main)}] runnung at {path}");
 
             FileManagerAdapter fileAdapter = new FileManagerAdapter(path);
@@ -42,6 +51,23 @@ namespace SortedStorage
             tcpServer.StartListening();
 
             Console.WriteLine($"[{nameof(Main)}] bye!!");
+        }
+
+        private static void SetupTelemetry(string instrumentationKey)
+        {
+            _events.Counters.Add(new EventCounterCollectionRequest(nameof(TcpServiceEventSource), "newClientsCounter"));
+            _events.Counters.Add(new EventCounterCollectionRequest(nameof(TcpServiceEventSource), "readBytesCounter"));
+            _events.Counters.Add(new EventCounterCollectionRequest(nameof(TcpServiceEventSource), "writeBytesCounter"));
+
+            _events.Counters.Add(new EventCounterCollectionRequest(nameof(SortedStorageApplicationEventSource), "updateDurationCounter"));
+            _events.Counters.Add(new EventCounterCollectionRequest(nameof(SortedStorageApplicationEventSource), "getDurationCounter"));
+
+
+            TelemetryConfiguration configuration = TelemetryConfiguration.CreateDefault();
+
+            configuration.InstrumentationKey = instrumentationKey;
+
+            _events.Initialize(configuration);
         }
     }
 }
